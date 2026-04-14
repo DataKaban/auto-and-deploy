39e1ac5bf25c84747dd71dbcb003ff5a7a348356
Yana_Nik <ykirillova@gmail.com>
2026-04-14 22:01:21 +0300

    v0.0

---
+++ generate-sales-data.py
@@ -0,0 +1,33 @@
+#Имитация данных о покупках и продажах акций за прошлый день.
+# Данные сохраняются в формате CSV.
+
+
+from datetime import datetime, timedelta
+import pandas as pd
+import configparser
+from random import randint 
+
+# создаем список акций
+config = configparser.ConfigParser() # создаем объект для чтения конфигурационного файла
+config.read('config.ini')
+COMPANIES = eval(config['Companies']['COMPANIES'])
+
+
+#получаем текущую дату
+today = datetime.today() 
+yesterday = today - timedelta(days=1)
+
+if 1 <= today.weekday() <= 5: 
+    #  Генерируем данные за вчерашний день.
+    
+    d = {
+            'dt': [yesterday.strftime('%d-%m-%Y') ] * len(COMPANIES)*2,
+            'company': COMPANIES * 2,
+            'transaction_type': ['buy'] * len(COMPANIES) + ['sell'] * len(COMPANIES),
+            'amount': [randint (0, 1000) for _ in range(len(COMPANIES)*2)]
+            
+        }
+
+df = pd.DataFrame(d)
+df.to_csv('sales-data.csv', index=False)
+
---
+++ run.py
@@ -0,0 +1,72 @@
+# Скрипт обработки продаж.
+# Cгенерированный файл продаж который генерируется файлом generate-sales-data.py будем обрабатывать  и заливать в базу данных на сервере.
+
+import os
+import pandas as pd
+import configparser
+from datetime import datetime, timedelta
+import yfinance as yf
+
+from pgdb import PGDatabase
+
+
+config = configparser.ConfigParser() # создаем объект для чтения конфигурационного файла
+config.read('config.ini')
+SALES_PATH = config['Files']['SALES_PATH']
+COMPANIES = eval(config['Companies']['COMPANIES'])
+DATABASE_CREDS = config['Database']
+
+#проверяем существует ли файл с данными о продажах, если да, то обрабатываем его и загружаем в базу данных, если нет, то выводим сообщение об ошибке.
+
+if os.path.exists(SALES_PATH):
+    sales_df = pd.read_csv(SALES_PATH)
+    print('Данные о продажах успешно загружены:')
+    os.remove(SALES_PATH) # удаляем файл после обработки, чтобы не обрабатывать его повторно при следующем запуске. 
+    print ("Файл удален после обработки.")
+else: 
+    sales_df = pd.DataFrame() # создаем пустой датафрейм, чтобы не возникало ошибок при попытке обработки несуществующего файла.    
+    print('Файл с данными о продажах не найден.')
+
+# Загрузка данных из yahoo finance.
+
+historical_d = {}
+
+for company in COMPANIES:
+    historical_d[company] = yf.download(
+        company,
+        start=(datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d"),
+        end=datetime.today().strftime("%Y-%m-%d"),
+    ).reset_index()
+    print(historical_d[company])
+
+# Загрузка данных о продажах в базу данных PostgreSQL    
+database = PGDatabase(
+    host= DATABASE_CREDS['HOST'],
+    port= DATABASE_CREDS['PORT'],
+    database= DATABASE_CREDS['DATABASE'],
+    user= DATABASE_CREDS['USER'],
+    password= DATABASE_CREDS['PASSWORD']
+    
+)
+for i, row in sales_df.iterrows():
+    query = f"insert into sales values ('{row['dt']}', '{row['company']}', '{row['transaction_type']}', {row['amount']})" # Removed float() cast
+    database.post(query, None) # Changed commit=True to commit=False
+
+
+# Загрузка данных о котировках в базу данных PostgreSQL
+for company, data in historical_d.items():
+    data = data.reset_index()
+    data.columns = data.columns.get_level_values(0)
+    for i, row in data.iterrows():
+        query = f"insert into stock (dt, company, open, close) Values (%s, %s, %s, %s)"
+        
+        values = (
+            row['Date'],
+            company,
+            row['Open'],
+            row['Close']
+        )
+        database.post(
+            query,
+            values
+        )
